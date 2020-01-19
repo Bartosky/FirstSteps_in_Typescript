17ee23447e629c35456642df82d47d42378712f7
Bartosz Malecki <ba.malecki@gmail.com>
2020-01-19 16:10:17 +0100

    typescript first steps

---
+++ greeter.js
@@ -0,0 +1,9 @@
+function greeter(person) {
+    return 'Hello' + person;
+}
+var user = 'Jan user';
+var user2 = [0, 1, 2, 3];
+var user3 = ;
+document.body.textContent = greeter(user);
+document.body.textContent = greeter(user2);
+document.body.textContent = greeter(user3);
---
+++ greeter.ts
@@ -0,0 +1,9 @@
+function greeter(person:string) {
+    return 'Hello' + person;
+}
+let user = 'Jan user';
+let user2 = [0,1,2,3];
+let user3 = ;
+document.body.textContent= greeter(user);
+document.body.textContent= greeter(user2);
+document.body.textContent= greeter(user3);
